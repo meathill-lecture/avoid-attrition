5e9aa91ab179f4438967c8794e7f36ac16f0c292
Meathill <meathill@gmail.com>
2018-03-10 11:57:26 +0800

    Change: update dependencies.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,13 +2,8 @@
  * Created by meathill on 2017/7/18.
  */
 
-import Uglify from 'uglifyjs-webpack-plugin';
-
 const config = require('./webpack.config');
 
 config.devtool = config.watch = false;
-config.plugins = [
-  new Uglify(),
-];
 
 module.exports = config;
